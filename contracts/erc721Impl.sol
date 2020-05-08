5b5d0a47a962dd7d38b99b1b4f1ebd0bf5331ee8
hoanghiep8921 <36073931+hoanghiep8921@users.noreply.github.com>
2020-05-08 17:33:38 +0700

    update

--- contracts/erc721Impl.sol
+++ contracts/erc721Impl.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0 <0.7.0;
 
-import "./erc721.sol";
+import "./ERC721.sol";
 
 contract ERC721Impl is ERC721 {
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var ERC721Impl = artifacts.require("ERC721Impl");
 
 module.exports = function(deployer) { 
-    deployer.deploy(ERC721Impl, "ERC721Impl");
+    deployer.deploy(ERC721Impl);
 };
